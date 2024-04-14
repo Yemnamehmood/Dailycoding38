39f004274bf957a83fda2eac70e819a292cf2365
YemnaMehmood <163342865+Yemnamehmood@users.noreply.github.com>
2024-04-14 19:42:24 +0500

    Dailycodingtask38
    Different functions have been performed.

---
+++ main.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//DAY 38
+//TASK 1 (Creating a Map)
+let countries = new Map();
+countries.set("Pakistan", "Islamabad");
+countries.set("China", "Beijing");
+countries.set("Australia", "Canberra");
+console.log(countries);
+//It shows the countries that we set in map and also shows their capitals.
+//TASK 2 (Using function to checking capital of canada) using if/else statement
+function logcapitalofcanada(countires) {
+    if (countires.has("Canada")) {
+        console.log(`The Capital Of Canada is ${countires.get("Canada")}`);
+    }
+    else {
+        console.log("Canada is not in the Map.");
+    }
+}
+//Assuming countries map from task 1 
+logcapitalofcanada(countries);
+//It shows the else statement only 
+//TASK 3 (Creating Map to store student IDs keys and names values)
+let students = new Map();
+students.set(1, "Yemna");
+students.set(2, "Shayan");
+students.set(3, "Amna");
+//using forEach method because (`forEach` in TypeScript: Iterate over array elements easily.)
+students.forEach((name, id) => {
+    console.log(`Student ID: ${id} , Name: ${name}`);
+});
